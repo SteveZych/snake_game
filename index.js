05cb619351384fede64018f87815b2f6051455b2
Steven Zych <stevezych@gmail.com>
2022-02-19 09:54:28 -0700

    basic layout and starter js

---
+++ index.js
@@ -0,0 +1,14 @@
+const board = document.getElementById("board");
+const highScore = document.getElementById("highScore");
+const currentScore = document.getElementById("currentScore");
+
+let filledBoard = [];
+
+
+
+
+function createBoard(){
+    for (let i = 0; i < 100; i++){
+        
+    }
+}
